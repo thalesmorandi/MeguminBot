937f223c100df36da8862c3f38ca043c1cae5173
Thales <thales.morandi@produtivit.com.br>
2021-08-02 14:22:47 -0300

    rebase

--- mec.py
+++
@@ -1,6 +0,0 @@
-mec = str
-print('-------------------------------------------------------------------------------------------------')
-print('Servidor Discord : https://discord.me/discordialol')
-print('Github Link: https://github.com/ThalesMorandi/MeguminBot')
-print('-------------------------------------------------------------------------------------------------')
-mec
